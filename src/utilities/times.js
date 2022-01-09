4072772aa4036b36f00d877aa192c134e16111fd
Max Tokman <maximetokman@gmail.com>
2022-01-09 02:17:47 -0600

    Implement change meet time

--- src/utilities/times.js
+++ src/utilities/times.js
@@ -2,6 +2,8 @@ export const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
 export const days = ['M', 'Tu', 'W', 'Th', 'F'];
 
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
 export const hasConflict = (course, selected) => (
     selected.some(selection => courseConflict(course, selection))
 );
@@ -27,3 +29,13 @@ export const getCourseTerm = course => (
     terms[course.id.charAt(0)]
 );
   
+export const timeParts = meets => {
+    const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+    return !match ? {} : {
+      days,
+      hours: {
+        start: hh1 * 60 + mm1 * 1,
+        end: hh2 * 60 + mm2 * 1
+      }
+    };
+  };
