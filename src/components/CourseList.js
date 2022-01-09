35955e6cfc2b84615ae1472b059f69aa863f8de0
Max Tokman <maximetokman@gmail.com>
2022-01-09 12:22:40 -0600

    Add authentication

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,15 +1,36 @@
 import { useState } from 'react';
 import { terms, getCourseTerm } from '../utilities/times.js';
 import Course from './Course.js';
+import { signInWithGoogle, signOut, useUserState } from '../utilities/firebase.js';
 
-const TermSelector = ({term, setTerm}) => (
-    <div className='btn-group'>
-      {
-        Object.values(terms)
-          .map(value => <TermButton key={value} setTerm={setTerm} term={value} checked={value === term} />)
-      }
-    </div>
+const SignInButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signInWithGoogle()}>
+    Sign In
+  </button>
 );
+
+const SignOutButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signOut()}>
+    Sign Out
+  </button>
+);
+
+const TermSelector = ({term, setTerm}) => {
+    const [user] = useUserState();
+    return (
+      <div className='btn-toolbar justify-content-between'>
+        <div className='btn-group'>
+          {
+            Object.values(terms)
+              .map(value => <TermButton key={value} setTerm={setTerm} term={value} checked={value === term} />)
+          }
+        </div>
+        {user ? <SignOutButton /> : <SignInButton />}
+      </div>
+    );
+};
 
 const TermButton = ({term, setTerm, checked}) => (
     <>
